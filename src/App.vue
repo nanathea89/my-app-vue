2f6b4a2e74ef465f012bfd9dd797d53ca58cd51d
nanathea <m.nanamulyana@gmail.com>
2023-01-03 21:46:57 +0700

    appnotify

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <h1>File Upload</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem consectetur sit cupiditate assumenda repellendus animi iusto voluptate tenetur, fuga asperiores, minima nobis at dolorem vero quas id ratione quos. Enim.
+  </p>
+  <app-notify alert-message="hai hai" tipe="warning" posisi="tl"></app-notify>
 </template>
 
 <style scoped>
@@ -25,5 +30,9 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+p{
+  color:red
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import AppNotify from './components/AppNotify.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('app-notify',AppNotify)
+app.mount('#app')
